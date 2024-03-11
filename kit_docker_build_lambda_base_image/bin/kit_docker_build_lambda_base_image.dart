f9d9bded64881d2a2007cca73b816e629027094e
pierreduv <pierreduv@hotmail.com>
2024-03-11 13:32:40 -0400

    First commit

---
+++ kit_docker_build_lambda_base_image/bin/kit_docker_build_lambda_base_image.dart
@@ -0,0 +1,5 @@
+import 'package:kit_docker_build_lambda_base_image/kit_docker_build_lambda_base_image.dart' as kit_docker_build_lambda_base_image;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kit_docker_build_lambda_base_image.calculate()}!');
+}
---
+++ kit_docker_build_lambda_base_image/test/kit_docker_build_lambda_base_image_test.dart
@@ -0,0 +1,8 @@
+import 'package:kit_docker_build_lambda_base_image/kit_docker_build_lambda_base_image.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
